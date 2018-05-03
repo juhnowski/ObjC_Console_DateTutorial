670834c0f9512070b83231af8414990a83ed4caf
Juhnowski <juhnowski@gmail.com>
2018-05-03 03:24:23 +0300

    Initial Commit

---
+++ DateTutorial/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DateTutorial
+//
+//  Created by SBT on 03/05/2018.
+//  Copyright © 2018 SBT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
